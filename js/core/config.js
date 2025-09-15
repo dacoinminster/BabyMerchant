8a30ce15d3953696e5e6afb9f6c2857136cf7719
J.R. Willett <rodneygordonthefifth@gmail.com>
2025-09-15 13:28:35 -0700

    Add config.js to fix missing file error

---
+++ js/core/config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Empty config stub committed to repo to avoid missing script errors in production.
+// Use a local untracked js/core/config.js for developer overrides (gitignored).
+// Examples:
+//   // window.SLOW_TRANSITIONS = true;
+//   // window.DEBUG_AFFINE = false;
+
+// var devMode = true;
